c18b3ec9179ee5e53dc40491c616dbd967418c6c
Eri Hatanaka <66687793+eri-Q@users.noreply.github.com>
2022-01-18 19:47:01 +0900

    複数形に

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
   
   belongs_to :member
   
-  has_many :order_detail
+  has_many :order_details
 
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status_method: { waiting_payment: 0, confirm_payment: 1, making: 2, preparing_sending: 3, sent: 4 }
