6b86a8da3c8b6c50925c1dcb7fcf11fbfea535f1
daisuke0925 <93876241+daisuke0925@users.noreply.github.com>
2022-01-18 08:18:26 +0000

    create-order_detail_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_071248) do
+ActiveRecord::Schema.define(version: 2022_01_18_072556) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 2022_01_18_071248) do
     t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "price"
+    t.integer "amount"
   end
 
   create_table "orders", force: :cascade do |t|
