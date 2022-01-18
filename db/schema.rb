e61a12261abe47ac10059a92daa421c64650f4d0
EC2 Default User <ec2-user@ip-172-31-1-104.ec2.internal>
2022-01-18 07:45:56 +0000

    address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_053852) do
+ActiveRecord::Schema.define(version: 2022_01_18_074251) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "postal_code"
+    t.string "residence"
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,6 +50,11 @@ ActiveRecord::Schema.define(version: 2022_01_18_053852) do
     t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
