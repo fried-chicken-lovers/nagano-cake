f6e16c70f8a8f546542dd2327557cd69fab1f68b
eri-Q <eri.hatanaka8@gmail.com>
2022-01-19 12:51:13 +0000

    add-items-registration

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,14 +7,24 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to root_path
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :price, :image_id, :is_deleted)
   end
 end
--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,5 +1,5 @@
 class Admin::MembersController < ApplicationController
-   before_action :authenticate_user!
+   before_action :authenticate_member!
    def index
       @members = Member.all.page(params[:page]).per(10)
    end
