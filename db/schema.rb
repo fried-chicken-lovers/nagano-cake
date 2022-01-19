a76ff1f2982fc66e64065a4a5302dc0952f50397
eri-Q <eri.hatanaka8@gmail.com>
2022-01-19 06:04:54 +0000

    members-index devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_01_18_090807) do
-
-
-
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -87,20 +83,8 @@ ActiveRecord::Schema.define(version: 2022_01_18_090807) do
   end
 
   create_table "orders", force: :cascade do |t|
-
-    t.string "postal_code"
-    t.string "adress"
-    t.string "name"
-    t.integer "shipping_cost"
-    t.integer "total_payment"
-    t.integer "status"
-    t.integer "payment_method"
-    
-    
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
   end
 
 end
