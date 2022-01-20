9ede3160973ce0290b91412c2f4e07b01ec59f12
Eri Hatanaka <66687793+eri-Q@users.noreply.github.com>
2022-01-20 15:57:08 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -91,8 +91,8 @@ ActiveRecord::Schema.define(version: 2022_01_20_064019) do
     t.integer "total_payment"
     t.integer "status"
     t.integer "payment_method"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
