84db1fea78a1cbee701415f5aef6c828e03178f2
eri-Q <eri.hatanaka8@gmail.com>
2022-01-20 12:05:49 +0000

    [Done]admin-members-edit

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -8,13 +8,20 @@ class Admin::MembersController < ApplicationController
    end
 
    def edit
+      @member = Member.find(params[:id])
    end
 
    def update
+      @member = Member.find(params[:id])
+      if @member.update(member_params)
+      redirect_to admin_member_path(@member)
+      else
+      render "edit"
+      end
    end
 
    private
    def member_params
-      params.require(:member).permit(:last_name, :first_name, :last_name_kana, :first_name_kana)
+      params.require(:member).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :email, :telephone_namber)
    end
 end
