5c91ebba2cda12014296a9140251fcab09a64daa
EC2 Default User <ec2-user@ip-172-31-1-104.ec2.internal>
2022-01-22 12:47:12 +0000

    order.history

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,24 +1,27 @@
 class Public::OrdersController < ApplicationController
-  
-  
+
+
   def new
   end
 
   def confirm
   end
-  
+
   def create
   end
-  
+
   def thanx
   end
-  
+
   def index
-    @member = current_member
-    @order = order.all
+    @orders = current_member.orders
+
   end
-  
+
   def show
   end
-  
+
+  def order_params
+    params.require(:order).permit(:postal_code, :adress, :name, :payment_method, :total_payment, :status, :shipping_cost)
+  end
 end
