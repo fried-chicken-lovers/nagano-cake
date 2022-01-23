25610c9b66541b4671366503b531fe1b6c9badf8
EC2 Default User <ec2-user@ip-172-31-1-104.ec2.internal>
2022-01-23 12:44:31 +0000

    order#comfirm

--- db/schema.rb
+++ db/schema.rb
@@ -97,8 +97,8 @@ ActiveRecord::Schema.define(version: 2022_01_23_044635) do
     t.integer "total_payment"
     t.integer "status"
     t.integer "payment_method"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer "member_id"
   end
 
