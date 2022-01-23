1e9e41410e09e10ad7e26d92b1a28cbbce751ab9
EC2 Default User <ec2-user@ip-172-31-1-104.ec2.internal>
2022-01-23 06:58:27 +0000

    create only

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,12 +2,32 @@ class Public::OrdersController < ApplicationController
 
 
   def new
+    @order = Order.new
+
   end
 
   def confirm
+    @order = Order.new
+    
   end
 
   def create
+    cart_items = current_member.cart.all
+    @order = current_member.orders.new(order_params)
+    
+    if @order.save
+      cart_items.each do |cart|
+        order_detail = OrderDetails.new
+        order_detail.item_id =  cart.item_id
+        order_detail.order_id = @order.id
+        order_detail.order_quantity = cart.quantity
+        order_detail.order_price = cart.item.price
+        order_detail.save
+      end
+      redirect_to public_orders_thanx_path
+      cart_items.destroy_all
+    end
+      
   end
 
   def thanx
