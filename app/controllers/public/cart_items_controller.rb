8c977a90e295bc78182e98792d0ddf1478a75408
EC2 Default User <93399430+kawa789@users.noreply.github.com>
2022-01-23 06:40:09 +0000

    ご注文ありがとう画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,30 +1,13 @@
 class Public::CartItemsController < ApplicationController
 
   def index
+    #@cart_item = Cart.find(params[:id])
      @cart_items = Cart.all
-
-     @sum = 0
-     @cart_items.each do |cart_item|
-      @subtotal = (Item.find(cart_item.item_id).price * 1.1 * cart_item.quantity).to_i
-      @sum += @subtotal
-     end
-
   end
 
   def create
     @cart_item = Cart.new(cart_params)
     @cart_item.member_id = current_member.id
-
-    @cart_item = current_member.carts.build(cart_params)
-    @cart_items = current_member.carts.all
-    @cart_items.each do |cart_item|
-     if cart_item.item_id == @cart_item.item_id
-      new_quantity = cart_item.quantity + @cart_item.quantity
-      cart_item.update_attribute(:quantity, new_quantity)
-      @cart_item.delete
-     end
-    end
-
     @cart_item.save
     redirect_to public_cart_items_path(@cart_item)
   end
@@ -35,15 +18,12 @@ class Public::CartItemsController < ApplicationController
     redirect_to public_cart_items_path(@cart_item)
   end
 
-  def destroy_all
-    current_member.carts.destroy_all
-    redirect_to public_cart_items_path
-  end
+  #def destroy_all
+    #Book.destroy_all
+    #current_user.books.destroy_all
+  #end
 
   def update
-    @cart_item = Cart.find(params[:id])
-    @cart_item.update(cart_params)
-    redirect_to public_cart_items_path(@cart_item)
   end
 
   private
